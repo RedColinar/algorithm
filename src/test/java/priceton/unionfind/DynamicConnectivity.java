1b1bacb6d5aef6f77a50b78ae113b475260626a2
RedColinar <1126525692@qq.com>
2019-03-28 00:14:36 +0800

    增加标准库

--- src/test/java/priceton/unionfind/DynamicConnectivity.java
+++ src/test/java/priceton/unionfind/DynamicConnectivity.java
@@ -1,8 +1,13 @@
 package priceton.unionfind;
 
+import princeton.unionfind.QuickFindUF;
+import princeton.unionfind.UF;
+
 public class DynamicConnectivity {
 
     public static void main(String[] args) {
-
+        UF uf = new QuickFindUF(10);
     }
+
+
 }
